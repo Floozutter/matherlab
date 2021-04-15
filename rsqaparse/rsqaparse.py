aa9cd46ef4d3758a605753178d66ab77ca82019a
Floozutter <me@floozutter.site>
2021-04-15 13:47:58 -0700

    this is awful

--- rsqaparse/rsqaparse.py
+++ rsqaparse/rsqaparse.py
@@ -8,14 +8,24 @@ from csv import DictWriter
 from pathlib import Path
 from argparse import ArgumentParser
 from sys import exit
+from re import compile, VERBOSE
 from typing import NamedTuple
+
+P_VERSION = compile(r"MRIQC version:\s*(.*)\s*\.")
+P_SUBJECTID = compile(r"Subject ID:\s*(.*)\s*\.")
+P_BIDSFILENAME = compile(r"BIDS filename:\s*(.*)\s*\.")
+#P_TSNR = compile(r"<tr>(?:(?!<tr)[\s\S])*tsnr(?:(?!<tr)[\s\S])*<td>\s*(.*)\s*<\/td>(?:(?!<tr)[\s\S])*<\/tr>")
+#P_TSNR = compile(r"<tr>(?:(?!<tr)[\s\S])*tsnr(?:(?!<tr)[\s\S])*<td>\s*(.*)\s*<\/td>\s*<\/tr>")
+#P_TSNR = compile(r"<tr>(?:(?!<tr)[\s\S])*tsnr[\s\S]*?<td>\s*(.*)\s*<\/td>\s*<\/tr>")
 
 class RestRow(NamedTuple):
     source: str
     subject: str
     session: str
+    acquisition: str
+    kind: str
     version: str
-    bidsfile: str
+    special: str
     tsnr: str
     fd_mean: str
     gcor: str
@@ -25,18 +35,41 @@ class RestRow(NamedTuple):
     def from_file(cls, filepath: Path) -> RestRow:
         # parse from path
         source = filepath.as_posix()
-        subject, session, *_ = filepath.stem.split("_")
+        subject, session, acquisition, kind, *_ = filepath.stem.split("_")
         # parse from html
-        version = ...
-        # init and return
-        return cls(...)
+        text = filepath.read_text()
+        version = P_VERSION.search(text).group(1)
+        if version == "0.11.0":
+            special = P_SUBJECTID.search(text).group(1)
+        elif version == "0.15.1":
+            special = P_BIDSFILENAME.search(text).group(1)
+        else:
+            raise ValueError(f"unexpected vesion `{version}`!")
+        tsnr = P_TSNR.search(text).group(1)
+        # instantiate and return
+        return cls(
+            source = source,
+            subject = subject,
+            session = session,
+            acquisition = acquisition,
+            kind = kind,
+            version = version,
+            special = special,
+            tsnr = tsnr,
+            fd_mean = fd_mean,
+            gcor = gcor,
+            gsr_x = gsr_x,
+            gsr_y = gsr_y
+        )
 
 class WholebrainRow(NamedTuple):
     source: str
     subject: str
     session: str
+    acquisition: str
+    kind: str
     version: str
-    bidsfile: str
+    special: str
     cnr: str
     cjv: str
     efc: str
@@ -46,10 +79,10 @@ class WholebrainRow(NamedTuple):
     def from_file(cls, filepath: Path) -> WholebrainRow:
         # parse from path
         source = filepath.as_posix()
-        subject, session, *_ = filepath.stem.split("_")
+        subject, session, acquisition, kind, *_ = filepath.stem.split("_")
         # parse from html
         version = ...
-        # init and return
+        # instantiate and return
         return cls(...)
 
 def parse_args() -> tuple[str, str, str]:
