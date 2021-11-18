ba020962eeb55cdb02458177bd3cc037d8203bc1
Floozutter <me@floozutter.site>
2021-11-18 10:02:33 -0800

    add sot_csv.py

---
+++ task-example-scripts/sot_csv.py
@@ -0,0 +1,31 @@
+import json
+import csv
+
+SPATIAL_ORIENTATION_DATA_PATH = "example-data/spatial-orientation.json"
+OUTPUT_CSV_PATH = "spatial-orientation.csv"
+
+with open(SPATIAL_ORIENTATION_DATA_PATH) as datafile:
+    datatext = datafile.read()
+timeline = json.loads(datatext)
+
+with open(OUTPUT_CSV_PATH, "w") as csvfile:
+    fieldnames = (
+        "trial_type",
+        "isRelevant",
+        "dateTime",
+        "completionReason",
+        "rt",
+        "targetRadians",
+        "responseRadians",
+    )
+    writer = csv.DictWriter(
+        csvfile,
+        fieldnames = fieldnames,
+        restval = "",
+        extrasaction = "ignore",
+        dialect = csv.unix_dialect,
+    )
+    writer.writeheader()
+    for trial in timeline:
+        if trial.get("trial_type") == "spatial-orientation" and trial.get("isRelevant", False):
+            writer.writerow(trial)
