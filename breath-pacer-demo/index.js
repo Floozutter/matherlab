06fbc077b9c1eec7e6b4cb0c9f0ef789232e505a
Floozutter <me@floozutter.site>
2021-11-25 19:59:21 -0800

    make the example pattern longer (and worse)

--- breath-pacer-demo/index.js
+++ breath-pacer-demo/index.js
@@ -1,11 +1,17 @@
 (() => {
     const PATTERN = [
-        {time: 1000, breathe: "in"},
-        {time: 2000, breathe: "out"},
-        {time: 4000, breathe: "in"},
-        {time: 7000, breathe: "hold"},
-        {time: 10000, breathe: "out"},
+        {time: 2000, breathe: "in"},
+        {time: 4000, breathe: "out"},
+        {time: 8000, breathe: "in"},
+        {time: 10000, breathe: "hold"},
+        {time: 15000, breathe: "out"},
     ];
+    for (let i = 0; i < 100; ++i) {
+        PATTERN.push({
+            time: 15000 + 5000*(1 - 0.8**(i+1))/(1 - 0.8),
+            breathe: i % 2 == 0 ? "in" : "out",
+        });
+    };
     const start = addBreathPacer(document.getElementById("demo-canvas"), PATTERN);
     document.getElementById("demo-button").addEventListener("click", () => {
         start();
